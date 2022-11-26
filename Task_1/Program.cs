ce4dfa1c9fda9fdd4b8138f72004b969d123015f
Funt <funt_88@mail.ru>
2022-11-26 22:36:12 +0300

    task_1 доработка до решения методами

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,14 +5,18 @@ int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers, 100, 999);
 WriteArray(numbers);
-int count = 0;
-for (int i = 0; i < numbers.Length; i++)
+void Count(int[] array)
 {
-    if (numbers[i] % 2 != 0)
-        continue;
-    count++;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+            continue;
+        count++;
+    }
+    Console.Write(count);
 }
-Console.Write(count);
+Count(numbers);
 void FillArrayRandomNumbers(int[] array, int min, int max)
 {
     for (int i = 0; i < array.Length; i++)
