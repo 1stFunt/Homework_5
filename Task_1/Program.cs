8c7f98c4be1135be1324844886f4696b247c8946
Funt <funt_88@mail.ru>
2022-11-26 20:58:47 +0300

    task_1 completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers, 100, 999);
+WriteArray(numbers);
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 != 0)
+        continue;
+    count++;
+}
+Console.Write(count);
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+void WriteArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+        continue;
+    Console.Write("[" + String.Join(", ", numbers) + "] -> ");
+}
